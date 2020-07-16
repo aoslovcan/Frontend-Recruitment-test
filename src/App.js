2df907d593b813ab8938f8a81ea3e403bf9699be
Antonija Oslovčan <oslovcana22@gmail.com>
2020-07-16 10:30:57 +0200

    Home page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/Navigation';
 
-import {library} from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-import{faHome, faAddressCard} from '@fortawesome/free-solid-svg-icons';
+import { faHome, faAddressCard } from '@fortawesome/free-solid-svg-icons';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './Routers/HomePage';
 import ContactUs from './Routers/ContactUs';
 
@@ -16,18 +16,16 @@ library.add(faHome, faAddressCard);
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navigation/>
-      <header className="App-header">
-      <Switch>
-      <Route path="/" exact component={HomePage}/>
-        <Route path="/home" exact component={HomePage}/>
-        <Route path="/contactus"  component={ContactUs}/>
+      <div className="App">
+        <Navigation />
 
-       
-      </Switch>
-      </header>
-    </div>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/home" exact component={HomePage} />
+          <Route path="/contactus" component={ContactUs} />
+        </Switch>
+
+      </div>
     </Router>
   );
 }
--- src/Routers/HomePage.js
+++ src/Routers/HomePage.js
@@ -1,14 +1,37 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { animations, easings } from 'react-animation';
 
 import './HomePage.css'
 
-class HomePage extends Component{
+const style = {
+    animation: `pop-in ${easings.easeOutExpo} 1200ms forwards`
+}
 
-    render(){
 
-        return(
-           <h1>Home page</h1>
-           )
+
+class HomePage extends Component {
+
+    render() {
+
+        return (
+
+            <>
+                <div className="cover" >
+
+                    <div className="inner" style={style}>
+                        <h1>Welcome to page</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                    </div>
+
+
+                </div>
+            </>
+
+        )
     }
 }
 
